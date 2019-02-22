c193f4938f5bddf498c05309db7fff7576caea74
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-21 20:34:41 -0500

    implemented item comparators, all tests passing

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,29 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    public int compare(Item o1, Item o2) {
+        double result = o1.getPrice()-o2.getPrice();
+        int resultToReturn = 0;
+
+        if(result < 0)
+        {
+            resultToReturn = -1;
+        }
+        else if(result > 0)
+        {
+            resultToReturn = 1;
+        }
+        else
+        {
+            resultToReturn = 0;
+        }
+        return resultToReturn;
+    }
 }
